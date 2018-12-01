c39bd8dfd35b4d222a7cc6f1a6ad52d2221c2b7e
honu <honu@nate.com>
2018-12-01 17:49:01 +0900

    Ejecting CRA, Adjusting Custom Sass

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styles from "./App.module.scss";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className={styles.app}>
+        <header className={styles.appHeader}>
+          <div>back btn icon</div>
+          <div>ui name</div>
+          <div>last modfied time</div>
+          <div>export icon</div>
+          <div>delete item</div>
+          <div>seach item</div>
+          <div>try it</div>
         </header>
+        <div className={styles.appContainer}>
+          <div className={styles.appSidebar}>side bar</div>
+          <div className={styles.appContent}>item editor</div>
+          <div className={styles.appPanel}>chat panel</div>
+        </div>
       </div>
     );
   }
